9b2fa9a50e8436dee4ad165f139f3a3ac6b9ecfa
kiashko777 <kiashko777@gmail.com>
2022-05-25 15:28:56 +0400

    feat:custom footer links and icons add
    custom footer links and icons created and styled

---
+++ app/code/Solwin/Instagram/Block/Widget/Instagram.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Instagram Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_Instagram
+ * @copyright  Copyright © 2006-2020 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+namespace Solwin\Instagram\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Instagram extends Template implements BlockInterface
+{
+    
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->setTemplate('widget/instagram.phtml');
+    }
+}
